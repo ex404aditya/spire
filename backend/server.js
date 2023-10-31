634130a085d43c8639924c55b96550436f5d4d1d
ex404aditya <ex404ug@gmail.com>
2023-10-31 15:12:58 +0530

    Adding messaging feature- backend files

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { v2 as cloudinary } from "cloudinary";
 //My routes
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 //Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () =>
   console.log(`server started at http://localhost:${PORT}`)
